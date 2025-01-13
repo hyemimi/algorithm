0cd33c508540c3c2e706be56291d676fdac20f35
이혜미 <103042868+hyemimi@users.noreply.github.com>
2025-01-13 14:11:48 +0900

    [Silver II] Title: 특정 거리의 도시 찾기, Time: 5688 ms, Memory: 224516 KB -BaekjoonHub

---
+++ 백준/Silver/18352. 특정 거리의 도시 찾기/특정 거리의 도시 찾기.py
@@ -0,0 +1,51 @@
+import heapq
+
+
+# 문제: x에서 출발해서 *최단 거리*가 k가 되는 정점 출력 
+# 도시의 갯수 n, 도로 갯수 m, 최단거리 k, 시작 노드 x
+n,m,k,x = map(int,input().split())
+graph= [[] for _ in range(n+1)]
+
+INF = int(1e9)
+dist = [INF] * (n+1) # 거리 저장 
+
+
+# 그래프 입력 (인접리스트)
+for i in range(1,m+1):
+    v1,v2 = map(int,input().split())
+
+    graph[v1].append(v2)
+
+
+
+pq = []
+heapq.heappush(pq,(x,0))
+dist[x] = 0
+
+
+while (pq):
+    node, edge = heapq.heappop(pq)
+    
+    # 이미 갱신됨 
+    if dist[node] < edge :
+        continue
+
+    for i in graph[node]:
+
+        cost = edge + 1
+
+        # 갱신
+        if cost < dist[i]:
+            dist[i] = cost
+            heapq.heappush(pq,(i,cost))
+
+isFind = False
+
+for i in range(1,n+1):
+    if dist[i] == k :
+        isFind = True
+        print(i)
+
+
+if isFind == False : print(-1)
+
