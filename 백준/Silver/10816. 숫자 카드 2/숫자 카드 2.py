5abda4b9fb87645d2f27f076d8675e966b9bbe6d
이혜미 <103042868+hyemimi@users.noreply.github.com>
2025-01-18 11:13:16 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 468 ms, Memory: 260500 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.py
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.py
@@ -1,22 +1,23 @@
 n = int(input())
-arr = list(map(int,input().split()))
+arr = list(map(int,input().split()))# O(n)
 m = int(input())
-arr2 = list(map(int,input().split()))
+arr2 = list(map(int,input().split())) # O(m)
 
 dict = {}
 
-# O(n^2)
+# O(n)
 for i in range(n):
-
+    
+    #O(1)
     if arr[i] in dict :
         dict[arr[i]] += 1
     else :
         dict[arr[i]] = 1
     
-# O(n^2)
+# O(m)
 for j in range(m):
 
-    s = dict.get(arr2[j])
+    s = dict.get(arr2[j]) # O(1)
     
     if s :
         print(s, end=' ')
