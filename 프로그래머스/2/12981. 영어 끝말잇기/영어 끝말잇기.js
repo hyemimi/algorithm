7ec633f5bb0c9d5b2ebe97c73a7c0a2f219f8669
이혜미 <103042868+hyemimi@users.noreply.github.com>
2025-08-11 21:06:42 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.16 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.js
@@ -0,0 +1,19 @@
+function solution(n, words) {
+
+    const usedWord = new Set();
+    const wordsLength = words.length;
+    let prevWord = words[0][0]
+    
+    for (let i=0; i<wordsLength; i++) {
+        const word = words[i];
+        
+        if (usedWord.has(word) || word[0] !== prevWord) {
+            return [i % n + 1, Math.floor(i / n) + 1]
+        }
+        
+        usedWord.add(word);
+        prevWord = word.slice(-1);
+    }
+
+    return [0,0];
+}
