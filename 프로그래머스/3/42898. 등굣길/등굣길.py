de72c8437aba7e47272042df96560d36ad387045
이혜미 <103042868+hyemimi@users.noreply.github.com>
2026-05-13 22:58:52 +0900

    [level 3] Title: 등굣길, Time: 1.13 ms, Memory: 9.34 MB -BaekjoonHub

---
+++ 프로그래머스/3/42898. 등굣길/등굣길.py
@@ -0,0 +1,22 @@
+
+def solution(m, n, puddles):
+    MOD = 1000000007
+
+    dp = [[0] * (m + 1) for _ in range(n + 1)]
+
+    puddle_set = set((y, x) for x, y in puddles)
+
+    dp[1][1] = 1
+
+    for i in range(1, n + 1):
+        for j in range(1, m + 1):
+            if (i, j) in puddle_set:
+                dp[i][j] = 0
+                continue
+
+            if i == 1 and j == 1:
+                continue
+
+            dp[i][j] = (dp[i - 1][j] + dp[i][j - 1]) % MOD
+
+    return dp[n][m]
